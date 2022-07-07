fe24fdc472df6d86d2fc15d5ee0f60bc45364ed1
Bunye, Hannah Rae <101611020+hannahrae-bunye-lspu@users.noreply.github.com>
2022-07-07 17:01:31 +0800

    Add files via upload

---
+++ php/pages/notlogged.php
@@ -0,0 +1,4 @@
+<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <li><a class="dropdown-item" name="register" href="register.php">Register</a></li>
+	<li><a class="dropdown-item" name="login" href="login.php">Login</a></li>
+</ul>
