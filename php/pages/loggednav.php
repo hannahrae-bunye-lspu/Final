3afc00c1e9045ae774c9bd4471716f41c2babdaa
Bunye, Hannah Rae <101611020+hannahrae-bunye-lspu@users.noreply.github.com>
2022-07-10 06:13:12 +0000

    Delete php directory

--- php/pages/loggednav.php
+++
@@ -1,6 +0,0 @@
-<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-	<li><a class="dropdown-item" name="register" href="register.php">Register</a></li>
-	<li><a class="dropdown-item" name="login" href="login.php">Login</a></li>
-	<li><a class="dropdown-item" name="profile" href="profile.php">Profile</a></li>
-	<li><a class="dropdown-item" name="logout" href="logout.php">Log out</a></li>
-</ul>
--- php/pages/notlogged.php
+++
@@ -1,4 +0,0 @@
-<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-    <li><a class="dropdown-item" name="register" href="register.php">Register</a></li>
-	<li><a class="dropdown-item" name="login" href="login.php">Login</a></li>
-</ul>
